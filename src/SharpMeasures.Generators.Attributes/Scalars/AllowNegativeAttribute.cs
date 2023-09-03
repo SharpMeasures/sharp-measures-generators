56d4a2888a44c18e3045a04dd1eafc17f3afb5f6
ErikWe <erikwestberg7@gmail.com>
2023-09-03 05:24:46 +0200

    Added AllowNegativeAttribute, useful if disallow negative is set as default

---
+++ src/SharpMeasures.Generators.Attributes/Scalars/AllowNegativeAttribute.cs
@@ -0,0 +1,11 @@
+﻿namespace SharpMeasures;
+
+using System;
+
+/// <summary>Applied to SharpMeasures scalar quantities, declaring that the marked quantity may be negative.</summary>
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct, AllowMultiple = false)]
+public sealed class AllowNegativeAttribute : Attribute
+{
+    /// <summary>Declares that the marked quantity may be negative.</summary>
+    public AllowNegativeAttribute() { }
+}
