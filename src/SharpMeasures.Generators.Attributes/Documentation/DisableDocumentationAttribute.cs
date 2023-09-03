7907767955f79a5b41a03e8140057ec529321b3f
ErikWe <erikwestberg7@gmail.com>
2023-09-03 05:22:35 +0200

    Separate enable/disable documentation attributes

---
+++ src/SharpMeasures.Generators.Attributes/Documentation/DisableDocumentationAttribute.cs
@@ -0,0 +1,11 @@
+﻿namespace SharpMeasures;
+
+using System;
+
+/// <summary>Applied to SharpMeasures types, declaring that XML documentation should not be generated for the marked type.</summary>
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct, AllowMultiple = false)]
+public sealed class DisableDocumentationAttribute : Attribute
+{
+    /// <summary>Declares that documentation should not be generated for the marked type.</summary>
+    public DisableDocumentationAttribute() { }
+}
---
+++ src/SharpMeasures.Generators.Attributes/Documentation/EnableDocumentationAttribute.cs
@@ -0,0 +1,11 @@
+﻿namespace SharpMeasures;
+
+using System;
+
+/// <summary>Applied to SharpMeasures types, declaring that XML documentation should be generated for the marked type.</summary>
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct, AllowMultiple = false)]
+public sealed class EnableDocumentationAttribute : Attribute
+{
+    /// <summary>Declares that documentation should be generated for the marked type.</summary>
+    public EnableDocumentationAttribute() { }
+}
